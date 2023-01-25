68e090f302c24d0b1129a7e6f1607a8c34d2c1c5
Dizzy <739091458@qq.com>
2023-01-25 21:07:24 +0800

    Update notice data

--- app/src/main/java/chat/cherish/xxim/core/listener/ReceivePushListener.java
+++ app/src/main/java/chat/cherish/xxim/core/listener/ReceivePushListener.java
@@ -5,7 +5,7 @@ import pb.Core;
 interface IReceivePush {
     void onPushMsgDataList(Core.MsgDataList msgDataList);
 
-    void onPushNoticeDataList(Core.NoticeDataList noticeDataList);
+    void onPushNoticeData(Core.NoticeData noticeData);
 }
 
 public abstract class ReceivePushListener implements IReceivePush {
@@ -14,6 +14,6 @@ public abstract class ReceivePushListener implements IReceivePush {
     }
 
     @Override
-    public void onPushNoticeDataList(Core.NoticeDataList noticeDataList) {
+    public void onPushNoticeData(Core.NoticeData noticeData) {
     }
 }
