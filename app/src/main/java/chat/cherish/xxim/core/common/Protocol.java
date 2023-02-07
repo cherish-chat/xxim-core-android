c9647ff1753d611f548c8ac1d4b74d74c41574fb
Dizzy <739091458@qq.com>
2023-02-08 00:50:31 +0800

    Update read and edit

--- app/src/main/java/chat/cherish/xxim/core/common/Protocol.java
+++ app/src/main/java/chat/cherish/xxim/core/common/Protocol.java
@@ -8,5 +8,7 @@ public class Protocol {
     public static String batchGetMsgListByConvId = "/v1/msg/batchGetMsgListByConvId"; // 批量获取消息列表
     public static String getMsgById = "/v1/msg/getMsgById"; // 获取消息
     public static String sendMsgList = "/v1/msg/sendMsgList"; // 发送消息列表
+    public static String sendReadMsg = "/v1/msg/sendReadMsg"; // 发送已读消息
+    public static String sendEditMsg = "/v1/msg/sendEditMsg"; // 发送编辑消息
     public static String ackNoticeData = "/v1/notice/ackNoticeData"; // 确认消费通知
 }
